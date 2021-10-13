3dc6616fce3aeeecc3bd9935fd74366aadb277d2
aygunestolga <aygunestolgaa@gmail.com>
2021-10-13 18:32:43 +0300

    Java 2D kütühhanesi ile oyun

---
+++ android/src/com/firstgame/game/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.firstgame.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.firstgame.game.PinkBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		initialize(new PinkBird(), config);
+	}
+}
